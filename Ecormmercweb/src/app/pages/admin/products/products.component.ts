cd7e808f1ea564078ac3ea16b3a1c8669dad040a
Sarangi11 <srnginimesha@gmail.com>
2024-06-24 15:28:59 +0530

    add

--- Ecormmercweb/src/app/pages/admin/products/products.component.ts
+++ Ecormmercweb/src/app/pages/admin/products/products.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  
+  isSidepanaleVisible:boolean = false;
+
+  openSidePanel(){
+    this.isSidepanaleVisible = true;
+  
+  }
+  closeSidePanel(){
+    this.isSidepanaleVisible = false;
+  }
+
 
 }
